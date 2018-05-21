ce1eb65e24abaef0c5979e81ee6c1076c1cc3937
RuiMACMonteiro <37885314+RuiMACMonteiro@users.noreply.github.com>
2018-05-21 13:36:21 +0100

    Add files via upload

---
+++ TheWall/Source/Code/CorePlugin/ILevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWall
+{
+
+    public interface ILevel
+    {
+        float Slow { get; }
+        float Normal { get; }
+        float High { get; }
+        float getVelocity();
+        void setVelocity(int level);
+    }
+}
